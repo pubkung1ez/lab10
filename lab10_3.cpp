612cad654fa60737cdd25813d6db5bb9012b3c6d
pubkung1ez <tirapan_30506@sysp.ac.th>
2025-12-23 16:08:55 +0700

    suss

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ifstream source;
+	source.open("score.txt") ;
+    cout << "Number of data = ";
+    cout << setprecision(3);
+    double y,z=0,a,b,c,d,e;
+    string x;
+    while(getline(source , x)){
+        y = y + stof(x);
+        b = b + (stof(x)*stof(x));
+        z++;
+    }
+    c= b/z;
+    a = y/z;
+    d = c - a*a;
+    e = sqrt(d);
+    cout << z << endl;
+    cout << "Mean = "<< a <<endl;
+    cout << "Standard deviation = "<< e;
+    source.close();
+	return 0;
+}
